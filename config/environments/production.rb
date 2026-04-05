4a022123d9633445241d7089acbcc625605a4d29
Andrey5353 <andreyae0117ab@gmail.com>
2026-04-05 23:23:52 +0300

    fix for production...to be continued...:(

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,6 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :primary } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -81,6 +80,4 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts << ".onrender.com"
-
-  config.solid_cable.connects_to = { database: { writing: :primary } }
 end
